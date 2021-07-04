e9f475399e06f4007e3ebd55896820c5fb58191b
Govorov Erkhaan <govoroverx@gmail.com>
2021-07-04 09:19:45 +0300

    Initial Commit

---
+++ playstationStoreClone/playstationStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  playstationStoreCloneApp.swift
+//  playstationStoreClone
+//
+//  Created by Govorov Erkhaan on 04.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct playstationStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
