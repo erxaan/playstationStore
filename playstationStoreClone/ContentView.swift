c2587bad096568c9dfdf958f14cfd8134e56a8d9
Govorov Erkhaan <govoroverx@gmail.com>
2021-07-05 09:26:15 +0300

    first setup

--- playstationStoreClone/ContentView.swift
+++ playstationStoreClone/ContentView.swift
@@ -7,15 +7,107 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct Game {
+    let title: String
+    let image: String
+    let percent: Int
+    let recieve: Int
+    let available: Int
+    var progress: CGFloat {
+        CGFloat(percent) / 100.0
     }
 }
+struct Stardew: View {
+    let game = Game(title: "Stardew Valley™",
+                    image: "stardew",
+                    percent: 70,
+                    recieve: 28,
+                    available: 40)
+    let radius: CGFloat = 100
+    
+    
+    var body: some View {
+        Spacer()
+        
+                
+                Text("Вы недавно играли")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                
+                Image(game.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                
+                VStack {
+                    Text(game.title)
+                        .font(.largeTitle)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Divider()
+                    NavigationLink(destination: Text("Призы")) {
+                        HStack {
+                            Text("Призы")
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }
+                        .foregroundColor(.white)
+                    }
+                    HStack(spacing: 50.0) {
+                        VStack(spacing: 10) {
+                            Text("\(game.recieve)")
+                                .font(.title)
+                            Text("Получено")
+                                .font(.footnote)
+                        } // Получено
+                        ZStack {
+                            Circle()
+                                .stroke(Color.gray, lineWidth: 3)
+                                .frame(width: radius, height: radius)
+                            Circle()
+                                .trim(to: game.progress)
+                                .stroke(Color.white, style: StrokeStyle(lineWidth: 3, lineCap: .round, lineJoin: .round))
+                                .frame(width: radius, height: radius)
+                                .rotationEffect(Angle(degrees: -90))
+                            Text("\(Int(game.percent))%")
+                        } // RingView
+                        VStack(spacing: 10) {
+                            Text("\(game.available)")
+                                .font(.title)
+                            Text("Доступно")
+                                .font(.footnote)
+                        } // Доступно
+                    }
+                    HStack(spacing: 30.0) {
+                        Image(systemName: "gamecontroller.fill")
+                        Image(systemName: "location.fill")
+                        Image(systemName: "bag.fill")
+                        Image(systemName: "gearshape.2.fill")
+                        Image(systemName: "magnifyingglass")
+                    }
+                    .font(.largeTitle)
+                    .padding(.vertical, 20)
+                    NavigationLink(
+                        destination: Image("mainmenu")){
+                        Text("Играть")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .underline()
+                    }}
+                    .padding(.horizontal)
+                Spacer()
+            }
+          
+        }
 
-struct ContentView_Previews: PreviewProvider {
+
+
+struct Stardew_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            Stardew()
+                .preferredColorScheme(.dark)
+            Stardew()
+                .preferredColorScheme(.light)
+        }
     }
 }
--- playstationStoreClone/playstationStoreCloneApp.swift
+++ playstationStoreClone/playstationStoreCloneApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct playstationStoreCloneApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Stardew()
         }
     }
 }
